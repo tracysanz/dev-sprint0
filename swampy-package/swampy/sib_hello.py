d783f3c102ce785bfebaf80570ce572067d38c0a
tracysanz <tracysanz@yahoo.com>
2013-03-07 21:11:27 -0500

    dev-sprint0

--- swampy-package/swampy/sib_hello.py
+++ swampy-package/swampy/sib_hello.py
@@ -7,12 +7,87 @@ from TurtleWorld import *
 world = TurtleWorld()			
 bob = Turtle()				
 
+world.clear()
 
+bob = Turtle()
 
-# This is where you put code to move bob
+def turn(turtle):
+  rt(turtle)
+  rt(turtle)
 
+def bigline(turtle):
+  fd(turtle, 100)
+  turn(turtle)   
 
+def nextletter(turtle):
+   pu(turtle)
+   fd(turtle,30)
+   pd(turtle)
+   
+pu(bob)
+bk(bob,150)
+pd(bob)
+lt(bob)
 
+bigline(bob)
+
+fd(bob,50)
+lt(bob)
+fd(bob,40)
+lt(bob)
+fd(bob,50)
+turn(bob)
+fd(bob,100)
+lt(bob)
+
+nextletter(bob)
+
+lt(bob)
+
+bigline(bob)
+lt(bob)
+fd(bob,40)
+turn(bob)
+fd(bob,40)
+lt(bob)
+fd(bob,50)
+lt(bob)
+fd(bob,40)
+turn(bob)
+fd(bob,40)
+lt(bob)
+fd(bob,50)
+lt(bob)
+fd(bob,40)
+
+pu(bob) 
+lt(bob)
+
+rt(bob)
+
+nextletter(bob)
+lt(bob)
+bigline(bob)
+bigline(bob)
+rt(bob)
+fd(bob,40)
+
+nextletter(bob)
+lt(bob)
+bigline(bob)
+bigline(bob)
+rt(bob)
+fd(bob,40)
+
+nextletter(bob)
+lt(bob)
+bigline(bob)
+lt(bob)
+fd(bob,40)
+rt(bob)
+bigline(bob)
+lt(bob)
+fd(bob,40)
 
 
 
