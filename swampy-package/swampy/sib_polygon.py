27ac0ad0a58b6ac6afe15811224c41044df48019
tracysanz <tracysanz@yahoo.com>
2013-03-19 11:43:28 -0400

    dev-sprint0

--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -3,19 +3,40 @@
 # Name:
 
 
-from TurtleWorld import * 		
+from TurtleWorld import * 	
+import math	
 world = TurtleWorld()			
 bob = Turtle()				
 
 
-
-# This is where you put code to move bob
-
-
-
-
+def square():
+	for i in range(4):
+		fd(bob,100)
+		lt(bob)
 
 
+def squaregen(turtle,len):
+	for i in range(4):
+		fd(turtle,len)
+		lt(turtle)	
 
+def polygon(turtle,len,sides,theta):
+	for i in range(sides):
+		fd(turtle,len)
+		lt(turtle,theta/sides)
+
+def circle(turtle,r):
+	circum = r * 2 * math.pi
+	sides = 60
+	theta = 360
+	polygon(turtle,circum/sides,sides,theta)
+
+def arc(turtle,r,theta):
+	circum = r * 2 * math.pi
+	sides = 60
+	polygon(turtle,circum/sides,sides,theta)
+
+
+arc(bob,30,150)
 
 wait_for_user()					
